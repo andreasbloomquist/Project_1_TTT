c1f097ae459efa7180bb53186decfc0b7dd8417f
andreasbloomquist <andreasbloomquist@woboinc.com>
2015-05-10 19:33:41 -0700

    Added js annotations and favicon

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-// OOP Tic Tac Toe boilerplate code
-
-// Execute this code only AFTER the document is ready
-// Hint: use jQuery's `$(document).ready
 $(document).ready(function(){
+  //Variables to hold text string for winner alerts, this felt easier to read than adding the strings in the alerts themselves.
+  var rebelWin = "The Rebel fighters have proven victorious against the evil Galatic Empire!"
+  var darkWin = "Darth Vader and the Galatic Empire have destroyed the Rebel Alliance."
 
+  //**************************************************************
+  // Game constructor
+  //**************************************************************
+  // The Game constructor is called to create a new game object
   function Game() {
+    //Create the board
+    this.board = new Board();
+
     //Creates new instances of players upon creation of a new game instance
     this.player1 = new Player("luke");
     this.player2 = new Player("darth");
     
     //Set the current player to player 1 so that we can get this show on the road!
-    this.currentPlayer = this.player1;
-    
-    //Create the board
-    this.board = new Board();  
+    this.currentPlayer = this.player1;  
+
+    this.gameOver = false;
+    this.turns = 0;
+    this.playerOneScore = 0;
+    this.playerTwoScore = 0;
   };
 
   //**************************************************************
@@ -24,15 +32,16 @@ $(document).ready(function(){
   // In addition, the setWinner() method increments the score counter for the given team, and dynamically updates the html score counter.
 
   Game.prototype.setWinner = function(winner){
-    this.board.gameOver = true;
-    alert(winner);
+    this.gameOver = true;
     if (winner === this.player1.team){
-      this.board.playerOneScore += 1;
-      $('#player1').html("rebel alliance: " + this.board.playerOneScore);
+      this.playerOneScore += 1;
+      $('#player1').html("rebel alliance: " + this.playerOneScore);
+      alert(rebelWin);
     } else {
-      this.board.playerTwoScore += 1;
-      $('#player2').html("galactic empire: " + this.board.playerTwoScore);
-    }
+      this.playerTwoScore += 1;
+      $('#player2').html("galactic empire: " + this.playerTwoScore);
+      alert(darkWin);
+    };
 };
   //**************************************************************
   // Prototype to check if there's a winner
@@ -69,81 +78,83 @@ $(document).ready(function(){
     };
   };
 
-
-  // Remember: prototypes are shared functions between all game instances
+  //**************************************************************
+  // Prototype to set the next player
+  //**************************************************************
+  // The nextPlayer method is called after each turn to update the "current player" to the proper value, and to increment the turn counter.
   Game.prototype.nextPlayer = function() {
-    //check to see if the game is over before allowing the next player to play
       //Switch current player to player 2
       var _this = this;
-      if (this.board.turns % 2 == 0) {
+      if (this.turns % 2 == 0) {
         this.currentPlayer = this.player2;
-        return this.board.turns += 1; 
-      //Switch current 
+        return this.turns += 1; 
+      //Switch current player to player 1
       } else {
         this.currentPlayer = this.player1;
-        return this.board.turns += 1;
+        return this.turns += 1;
       };
   };
 
-  // `Game.prototype.init` kicks off a new game with a board and two players
+  //**************************************************************
+  // Prototype to initialize the game
+  //**************************************************************
+  // After a new Game instance is created, the init method starts to listen for clicks to start the game. 
+  // When a click event occurs on an element with class="box" (aka a space on the board), a check is performed to ensure the game is not over, and to make sure the space hasn't already been clicked.
+  // If the game is not over, and no one has previously clicked the square then the team of the user who clicked the square is added as a class. This class enables the css to display the background image associated with the player.
+  // After adding the players name as a class to the html element, the checkWinner() and nextPlayer() methods are called.
   Game.prototype.init = function() {
     var _this = this;
+    game.resetGame();
     $(".box").on("click", function(event) {
-      if (!$(event.target).hasClass(_this.player1.team || _this.player2.team) && (_this.board.gameOver == false)) {
+      if (!$(event.target).hasClass(_this.player1.team || _this.player2.team) && (_this.gameOver == false)) {
         $(event.target).addClass(_this.currentPlayer.team);
           _this.checkWinner(_this.currentPlayer.team);
           _this.nextPlayer();
       };
   });
 };
-  //Reset button prototype
+  //**************************************************************
+  // Prototype to reset the game
+  //**************************************************************
+  // The resetGame() method starts an event listener on the reset button (#reset).
+  // When a click occurs on the reset button, the function removes classes added when a player clicked the square.
+  // In addition, the turn counter, and current player are reset and gameOver is set to false.
   Game.prototype.resetGame = function(){
       var _this = this;
-      $('#reset').on('click', function() {  
+      $('#reset').click(function() {
         $('.box').removeClass(_this.player1.team).removeClass(_this.player2.team);
-        _this.board.turns = 0;
-        _this.board.gameOver = false;
+        _this.turns = 0;
+        _this.gameOver = false;
         _this.currentPlayer = _this.player1;
       });
   };
 
-  // A starter Player constructor.
+  //**************************************************************
+  // Player constructor
+  //**************************************************************
+  // This constructor creates a new Player object, and takes the parameter "team" to set the team that the player plays on.
   function Player(team) {
       this.team = team;
 };
-  // A starter Board constructor.
+  
+  //**************************************************************
+  // Board constructor
+  //**************************************************************
+  // This constructor creates the necessary HTML elements to play the game including the board, the reset button, and scoring section. 
+  // While this constructor can create a board on the fly, some HTML elements must be hard coded to ensure the jQuery selectors know where to add the elements.
   function Board() {
-    //**************************************************************
-    //Do these make sense to have in the board constructor or game?
-    //**************************************************************
-    this.gameOver = false;
-    this.turns = 0;
-    this.playerOneScore = 0;
-    this.playerTwoScore = 0;
-
-    //***************************
-    // Setup HTML for game board
-    //***************************
     // First create reset button
     $('#reset').append('<button>reset game</button>');
-
     // Second create game board
     for (var i=0; i<9; i++){
       $('#board').append("<div class='box' id='" + (i+1) + "''>&nbsp;</div>");
     };
-
     //Setup score section
     $('#score').append('<p class="score" id="player1">rebel alliance: 0</p>').append('<p class="score" id="player2">galactic empire: 0</p>');
-  
   };
 
-  Game.prototype.setUpBoard = function(){
-    // QUESTION: I'm not sure if it makes sense to setup the html elements of the board using jquery
-  };
   // Start the game!
   var game = new Game();
-  game.setUpBoard();
   game.init();
-  game.resetGame();
 
 });
